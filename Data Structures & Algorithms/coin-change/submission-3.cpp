36dbef7143cd1a5e7b7c68ba5cd8c4c53252c296
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-04-03 00:01:46 +0300

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private: 
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        std::vector<int> dp(amount + 1, 1e9);
+        dp[amount] = 0;
+        for(int i{amount}; i>0; --i) {
+            for(int coin : coins) {
+                if (i - coin >= 0){
+                    dp[i - coin] = std::min(dp[i - coin], dp[i] + 1);
+                }
+            }
+        }
+        if (dp[0] == 1e9) return -1;
+        return dp[0];
+    }
+};
