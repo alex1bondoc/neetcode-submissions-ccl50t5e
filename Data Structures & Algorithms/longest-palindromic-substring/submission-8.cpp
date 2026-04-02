8e4aa5291c6b9e37dc73819ab3fbf59a1853f807
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-04-02 19:05:36 +0300

    Add: longest-palindromic-substring - submission-8

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-8.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n == 1) {
+            return s;
+        }
+        bool dp[n][n]{};
+        int sol = 0;
+        int solLength = 0;
+        for (int len{1}; len <= n; ++len) {
+            for(int i = 0; i < n - len + 1; ++i) {
+                int j = i + len - 1;
+                if (s[i] == s[j] && (len <= 2 || dp[i + 1][j - 1])){
+                    dp[i][j] = true;
+                    sol = i;
+                    solLength = len;
+                }
+            }
+        }
+        std::cout << sol << " " << solLength;
+        return s.substr(sol, solLength);
+    }
+};
