62e36f3bc829c6dca1974fe553dad3e606b32eeb
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-04-05 13:32:06 +0300

    Update: swim-in-rising-water - submission-1

--- Data Structures & Algorithms/swim-in-rising-water/submission-1.cpp
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size(), node = 0;
-        vector<int> dist(n, 100000000);
-        vector<bool> visit(n, false);
-        int edges = 0, res = 0;
-
-        while (edges < n - 1) {
-            visit[node] = true;
-            int nextNode = -1;
-            for (int i = 0; i < n; i++) {
-                if (visit[i]) continue;
-                int curDist = abs(points[i][0] - points[node][0]) +
-                               abs(points[i][1] - points[node][1]);
-                dist[i] = min(dist[i], curDist);
-                if (nextNode == -1 || dist[i] < dist[nextNode]) {
-                    nextNode = i;
-                }
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<bool>> visit(n, vector<bool>(n, false));
+        int minH = grid[0][0], maxH = grid[0][0];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                maxH = max(maxH, grid[row][col]);
+                minH = min(minH, grid[row][col]);
             }
-            res += dist[nextNode];
-            node = nextNode;
-            edges++;
         }
-        return res;
+
+        int l = minH, r = maxH;
+        while (l < r) {
+            int m = (l + r) >> 1;
+            if (dfs(grid, visit, 0, 0, m)) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+            for (int row = 0; row < n; row++) {
+                fill(visit[row].begin(), visit[row].end(), false);
+            }
+        }
+        return r;
+    }
+
+private:
+    bool dfs(vector<vector<int>>& grid, vector<vector<bool>>& visit,
+                                        int r, int c, int t) {
+        if (r < 0 || c < 0 || r >= grid.size() ||
+            c >= grid.size() || visit[r][c] || grid[r][c] > t) {
+            return false;
+        }
+        if (r == grid.size() - 1 && c == grid.size() - 1) {
+            return true;
+        }
+        visit[r][c] = true;
+        return dfs(grid, visit, r + 1, c, t) ||
+               dfs(grid, visit, r - 1, c, t) ||
+               dfs(grid, visit, r, c + 1, t) ||
+               dfs(grid, visit, r, c - 1, t);
     }
 };
