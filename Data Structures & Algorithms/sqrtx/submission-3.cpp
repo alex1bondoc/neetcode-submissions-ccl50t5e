57a56e7fc9e06fec72131a7c648fe4408eed103f
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-04-11 19:48:18 +0300

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long left = 0;
+        long long right = x;
+        long long ans = 0;
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            if (mid * mid == x) {
+                return mid;
+            }
+            else if (mid * mid < x) {
+                left = mid + 1;
+                ans = mid;
+            }
+            else {
+                right =  mid - 1;
+            }
+        }
+        return ans;
+    }
+};
