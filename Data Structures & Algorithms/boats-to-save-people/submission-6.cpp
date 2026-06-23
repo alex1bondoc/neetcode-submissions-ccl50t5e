62379ba2dff95ad42dd462f197598ec66efb900c
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-06-23 17:49:18 +0300

    Add: boats-to-save-people - submission-6

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-6.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int sol{};
+        std::sort(people.begin(), people.end());
+        int left = 0;
+        int right = people.size() - 1;
+        while (left <= right) {
+            int boat{people[right]};
+            right --;
+            if (boat + people[left] <= limit) {
+                left ++;
+            }
+            sol ++;
+        }
+        return sol;
+    }
+};
