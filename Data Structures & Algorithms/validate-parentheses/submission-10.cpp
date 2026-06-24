c9ef3e31ad9bc0f4a90a9365653801e77797b735
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-06-24 21:01:26 +0300

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> st;
+        for (const auto& c : s) {
+            if (c == ')') {
+                if (st.empty() || st.top() != '(') return false;
+                st.pop();                
+            }
+            else if (c == ']') {
+                if (st.empty() || st.top() != '[') return false;
+                st.pop();
+            }
+            else if (c == '}') {
+                if (st.empty() || st.top() != '{') return false;
+                st.pop();
+            } 
+            else {
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
