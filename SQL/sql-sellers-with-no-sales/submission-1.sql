e81094a85b422bdeb7125ec6ccbc238770e2e089
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-06-26 01:37:59 +0300

    Add: sql-sellers-with-no-sales - submission-1

---
+++ SQL/sql-sellers-with-no-sales/submission-1.sql
@@ -0,0 +1,4 @@
+select seller_name 
+from seller s
+where 0 = (select count(*) from orders o where o.seller_id = s.seller_id and o.sale_date >= '2020-01-01' AND o.sale_date <= '2020-12-31')
+order by seller_name;
