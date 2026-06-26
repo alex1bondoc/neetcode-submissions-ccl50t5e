5466ce52a6d15f4429f049c3a52a70c1710a6da3
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-06-27 01:00:44 +0300

    Add: car-fleet - submission-18

---
+++ Data Structures & Algorithms/car-fleet/submission-18.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        std::vector<std::pair<int, int>> cars;
+        for (int i{}; i < position.size(); ++i) {
+            cars.push_back({position[i], speed[i]});
+        }
+        std::sort(cars.begin(), cars.end(), [](const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
+            return p1.first > p2.first;
+        });
+        std::vector<double> sol;
+        for (const auto& car : cars) {
+            double time{(double) (target - car.first) / car.second};
+            sol.push_back(time);
+            while (sol.size() > 1 && sol[sol.size() - 1] <= sol[sol.size() - 2]) sol.pop_back();
+        }
+        return sol.size();
+    }
+};
