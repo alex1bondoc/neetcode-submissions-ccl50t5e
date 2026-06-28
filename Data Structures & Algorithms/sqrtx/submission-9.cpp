4f0c3b3afbae47c45ba23ea6d8091571346092eb
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-06-28 22:52:09 +0300

    Add: sqrtx - submission-9

---
+++ Data Structures & Algorithms/sqrtx/submission-9.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x){
+        int sol {};
+        int left{};
+        int right{x};
+        while (left <= right) {
+            int mid{left + (right - left) / 2};
+            if ((long long)mid * mid <= x) {
+                sol = mid;
+                left = mid + 1;
+            }
+            else right = mid - 1;
+        }
+        return sol;
+    }
+};
