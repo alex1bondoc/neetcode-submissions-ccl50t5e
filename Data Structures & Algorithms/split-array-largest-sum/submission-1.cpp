2c324f7980e38af115681ca99e10bbb8fdeaa6c1
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-07-02 13:26:59 +0300

    Add: split-array-largest-sum - submission-1

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int left{std::max_element(nums.begin(), nums.end())[0]};
+        int right{std::accumulate(nums.begin(), nums.end(), 0)};
+        int sol{1 << 30};
+        while (left <= right) {
+            int mid{left + (right - left) / 2};
+            int subs{1};
+            int sum{};
+            for (const auto& num : nums) {
+                if (sum + num > mid) {
+                    subs++;
+                    sum = num;
+                }
+                else sum += num;
+                if (subs > k) break;
+            }
+            if (subs <= k) {
+                sol = std::min(sol, mid);
+                right = mid - 1;
+            }
+            else left = mid + 1;
+        }
+        return sol;
+    }
+};
