1f21633f6760b279107824961bc8bff8ffa70a13
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-07-24 15:44:07 +0300

    Add: kth-largest-integer-in-a-stream - submission-6

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-6.cpp
@@ -0,0 +1,19 @@
+class KthLargest {
+    priority_queue<int, vector<int>, greater<int>> pq;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        for (const auto& n : nums) {
+            if (pq.size() == k) {
+                pq.pop();
+            }
+            pq.push(n);
+        }
+    }
+    
+    int add(int val) {
+        if (pq.size())
+            pq.pop();
+        pq.push(val);
+        return pq.top();
+    }
+};
