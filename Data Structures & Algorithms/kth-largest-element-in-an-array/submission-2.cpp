0b4cf414b84e4a822cffd0b94d614b394eefd1e9
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-04 21:45:58 +0300

    Add: kth-largest-element-in-an-array - submission-2

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int left{}, right{static_cast<int>(nums.size() - 1)};
+        int pivot{static_cast<int>(nums.size())};
+        while (k != pivot) {
+            int l{left};
+            for (int r{left}; r < right; ++r) {
+                if (nums[r] < nums[right]) {
+                    swap(nums[r], nums[l]);
+                    l++;
+                }
+            }
+            swap(nums[l], nums[right]);
+            pivot = l;
+            if (pivot < k) left = pivot + 1;
+            else if (pivot > k) right = pivot - 1;
+        }
+        return nums[nums.size() - k];
+    }
+};
