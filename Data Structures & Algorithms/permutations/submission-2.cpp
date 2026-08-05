a7df816a8f11f5363508c7e1dcb15c64ec96d662
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-05 17:39:22 +0300

    Update: permutations - submission-2

--- Data Structures & Algorithms/permutations/submission-2.cpp
+++ Data Structures & Algorithms/permutations/submission-2.cpp
@@ -1,30 +1,23 @@
 class Solution {
+    vector<vector<int>> res{};
+    unordered_set<int> set;
 public:
-    vector<vector<int>> res;
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        res.clear();
-        sort(candidates.begin(), candidates.end());
-        dfs(0, {}, 0, candidates, target);
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> cur;
+        backtrack(cur, nums, 0);
         return res;
     }
-
-private:
-    void dfs(int idx, vector<int> path, int cur, vector<int>& candidates, int target) {
-        if (cur == target) {
-            res.push_back(path);
-            return;
+    void backtrack(vector<int>& cur, vector<int>& nums, int k) {
+        if (k == nums.size()) {
+            res.push_back(cur);
         }
-        for (int i = idx; i < candidates.size(); i++) {
-            if (i > idx && candidates[i] == candidates[i - 1]) {
-                continue;
-            }
-            if (cur + candidates[i] > target) {
-                break;
-            }
-
-            path.push_back(candidates[i]);
-            dfs(i + 1, path, cur + candidates[i], candidates, target);
-            path.pop_back();
+        for (int i{}; i < nums.size(); ++i) {
+            if (set.contains(i)) continue;
+            set.insert(i);
+            cur.push_back(nums[i]);
+            backtrack(cur, nums, k + 1);
+            cur.pop_back();
+            set.erase(i);
         }
     }
 };
