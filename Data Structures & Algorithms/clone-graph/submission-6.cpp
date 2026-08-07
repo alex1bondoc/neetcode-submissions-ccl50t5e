cb605a77ac78b54a193157a643b729b81df84f81
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-07 21:07:07 +0300

    Add: clone-graph - submission-6

---
+++ Data Structures & Algorithms/clone-graph/submission-6.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        unordered_map<Node*, Node*> map;
+        unordered_set<Node*> visited;
+        queue<Node*> q;
+        map[node] = new Node(node->val);
+        q.push(node);
+        visited.insert(node);
+        while (q.size()) {
+            auto n = q.front();
+            q.pop();
+            for (const auto neighbor : n->neighbors) {
+                if (!map.contains(neighbor)) {
+                    map[neighbor] = new Node(neighbor->val);
+                    q.push(neighbor);
+                }
+                map[n]->neighbors.push_back(map[neighbor]);
+                
+            }
+        }
+        return map[node];
+    }
+};
