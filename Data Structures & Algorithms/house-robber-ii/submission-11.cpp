8504ba0c7a9b37f957fdbb2d85cd108144891cf1
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-08 15:02:42 +0300

    Add: house-robber-ii - submission-11

---
+++ Data Structures & Algorithms/house-robber-ii/submission-11.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    unordered_map<int, int> cache;
+public:
+    int rob(vector<int>& nums) {
+        int n{static_cast<int>(nums.size())};
+        if (n == 1) return nums[0];
+        auto back = [&](auto self, int i, int end){
+            if (i >= end) {
+                return 0;
+            }
+            if (!cache.contains(i)) 
+                cache[i] = max(self(self, i + 1, end), self(self, i + 2, end) + nums[i]);
+            return cache[i];
+        };
+        int res{back(back, 0, n - 1)};
+        cache.clear();
+        return max(res, back(back, 1, n));
+    }
+};
