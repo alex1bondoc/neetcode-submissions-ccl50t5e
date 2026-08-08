a83bb161582e71f9822d99a6cd9a77f23c3a7e6b
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-08 12:19:06 +0300

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> in(numCourses);
+        int counter{};
+        for (const auto& pre : prerequisites) {
+            in[pre[1]]++;
+        }
+        queue<int> q;
+        for (int i{}; i < numCourses; ++i) {
+            if (in[i] == 0) q.push(i);
+        }
+        while (q.size()) {
+            counter++;
+            int node{q.front()};
+            q.pop();
+            for (const auto& pre: prerequisites) {
+                if (pre[0] == node) {
+                    in[pre[1]]--;
+                    if (in[pre[1]] == 0) q.push(pre[1]);
+                }
+            }
+        }
+        return counter == numCourses ? true : false;
+    }
+};
