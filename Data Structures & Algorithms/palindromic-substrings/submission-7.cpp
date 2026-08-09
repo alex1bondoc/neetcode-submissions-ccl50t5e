16456ec31a13af745fd7f7d4b3d667d8526fc6b6
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-09 15:38:16 +0300

    Add: palindromic-substrings - submission-7

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-7.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        string t{"#"};
+        for (char c : s) {
+            t += c;
+            t += "#";
+        }
+        int n{static_cast<int>(t.size())};
+        vector<int> p(n + 1);
+        int l{}, r{};
+        for (int i{}; i < n; ++i) {
+            p[i] = (i < r) ? min(r - i, p[l + r - i]) : 0;
+            while (i + p[i] + 1 < n && i - p[i] - 1 >= 0 && t[i - p[i] - 1] == t[i + p[i] + 1]) p[i] ++;
+            if (i + p[i] + 1 > r) {
+                r = i + p[i] + 1;
+                l = i - p[i] - 1;
+            }
+        }
+        int res{};
+        for (int i : p) {
+            if (i) res += (i + 1) / 2;
+        }
+        return res;
+    }
+};
