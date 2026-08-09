121f868b66cfb676b0159cb466b7a04cfb21b0e8
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-09 16:41:15 +0300

    Add: longest-increasing-subsequence - submission-15

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-15.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n{static_cast<int>(nums.size())};
+        vector<int> dp(n + 1, 1);
+        dp[n] = 0;
+        for (int i{n - 1}; i >= 0; i--) {
+            for (int j{i + 1}; j < n; ++j) {
+                if (nums[i] < nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            } 
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
