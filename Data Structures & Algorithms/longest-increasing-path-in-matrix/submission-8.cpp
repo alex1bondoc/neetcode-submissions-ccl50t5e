449fc339f3fcb2ca1c769779ec4fcf804ca34dbc
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-17 16:39:39 +0300

    Add: longest-increasing-path-in-matrix - submission-8

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-8.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n{static_cast<int>(matrix.size())}, m{static_cast<int>(matrix[0].size())};
+        vector<vector<int>> dp(n, vector<int> (m));
+        int res{};
+        auto dfs = [&](auto self, int i, int j, int prev) {
+            if (i < 0 || i >= n || j < 0 || j >= m || matrix[i][j] <= prev) return -1;
+
+            if (dp[i][j] != 0) return dp[i][j];
+            dp[i][j] = 1;
+            dp[i][j] = max(dp[i][j], self(self, i + 1, j, matrix[i][j]) + 1);
+            dp[i][j] = max(dp[i][j], self(self, i - 1, j, matrix[i][j]) + 1);
+            dp[i][j] = max(dp[i][j], self(self, i, j + 1, matrix[i][j]) + 1);
+            dp[i][j] = max(dp[i][j], self(self, i, j - 1, matrix[i][j]) + 1);
+            return dp[i][j];
+        };
+        for (int i{}; i < n; ++i) {
+            for (int j{}; j < n; ++j) {
+                res = max(res, dfs(dfs, i, j, -1));
+            }
+        }
+        return res;
+    }
+};
