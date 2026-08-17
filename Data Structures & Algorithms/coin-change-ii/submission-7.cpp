995d44ee756338babd275c28b230be8932868830
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-17 14:54:15 +0300

    Add: coin-change-ii - submission-7

---
+++ Data Structures & Algorithms/coin-change-ii/submission-7.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n{static_cast<int>(coins.size())};
+        sort(coins.begin(), coins.end());
+        vector<vector<int>> dp(n + 1, vector<int> (amount + 1, -1));
+        auto back = [&](auto self, int i, int a) {
+            if (i == n) {
+                if (amount == a) return 1;
+                return 0;
+            }
+            if (a > amount) return 0;
+            if (dp[i][a] != -1) return dp[i][a];
+            dp[i][a] = self(self, i, a + coins[i]) + self(self, i + 1, a);
+            return dp[i][a];
+        
+        };
+        return back(back, 0, 0);
+    }
+};
