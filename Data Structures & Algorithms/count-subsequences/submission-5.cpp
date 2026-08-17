e31d83212eacb6135817dcc7187b07d5dbee6671
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-17 16:59:11 +0300

    Add: count-subsequences - submission-5

---
+++ Data Structures & Algorithms/count-subsequences/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n{static_cast<int>(s.size())}, m{static_cast<int>(t.size())};
+        
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1));
+        dp[0][0] = 1;
+        for (int i{1}; i <= n; ++i) {
+            dp[i][0] = 1; 
+            for (int j{1}; j <= m; ++j) {
+                dp[i][j] = dp[i - 1][j];
+                if (s[i - 1] == t[j - 1]) dp[i][j] += dp[i - 1][j - 1];
+            }
+        }
+        // {
+        //     i == n && j == m return 1
+        //     if i > n || j > m return 0
+        //     return max(self(i + 1, j), self(i, j + 1))
+
+        // }
+        return dp[n][m];
+    }
+};
