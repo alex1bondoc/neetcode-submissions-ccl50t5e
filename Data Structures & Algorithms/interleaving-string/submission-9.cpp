d57620997d26bb1543f74910fa00443988cc4e61
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-17 15:56:02 +0300

    Add: interleaving-string - submission-9

---
+++ Data Structures & Algorithms/interleaving-string/submission-9.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s3.size() != s1.size() + s2.size()) return false;
+        vector<vector<int>> dp(s1.size() + 1, vector<int> (s2.size() + 1));
+        // auto back = [&](auto self, int i, int j){
+        //     if (i == s1.size() && j == s2.size()) {
+        //         return true;
+        //     }
+        //     else if (i > s1.size() || j > s2.size()) return false;
+        //     bool res = false;
+        //     if (dp[i][j] == -1) {
+        //         if (s1[i] == s3[i + j]) res = res || self(self, i + 1, j);
+        //         if (s2[j] == s3[i + j]) res = res || self(self, i, j + 1);
+        //         if (res) dp[i][j] = 1;
+        //         else dp[i][j] = 0;
+        //     }
+        //     return dp[i][j] == 1;
+            
+        // };
+        int n{static_cast<int>(s1.size())}, m{static_cast<int>(s2.size())};
+        for (int i{}; i <= n; ++i) {
+            dp[i][m] = 1;
+        }
+        for (int j{}; j <= m; ++j) {
+            dp[n][j] = 1;
+        }
+        for (int i{n - 1}; i >= 0; i-- ) {
+            for (int j{m - 1}; j >= 0; j--) {
+                if (s1[i] == s3[i + j]) dp[i][j] = dp[i][j] || dp[i + 1][j];
+                if (s2[j] == s3[i + j]) dp[i][j] = dp[i][j] || dp[i][j + 1];
+            }
+        }
+        return dp[0][0];
+    }
+};
