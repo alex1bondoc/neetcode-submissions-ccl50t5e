63885b2e181aa0818358b123edfce150999fbfc8
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-20 16:35:29 +0300

    Add: network-delay-time - submission-11

---
+++ Data Structures & Algorithms/network-delay-time/submission-11.cpp
@@ -0,0 +1,33 @@
+#include <limits>
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        const int INF{std::numeric_limits<int>::max() / 2};
+        vector<vector<pair<int, int>>> adj(n);
+        for (const auto& time : times) {
+            adj[time[0] - 1].push_back({time[1] - 1, time[2]});
+        }
+        vector<int> distances(n, INF);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, k - 1});
+        distances[k - 1] = 0;
+        while (pq.size()) {
+            auto [dis, node] = pq.top();
+            pq.pop();
+
+            for (const auto& [nei, d] : adj[node]) {
+                if (distances[nei] > d + dis) {
+                    pq.push({d + dis, nei});
+                    distances[nei] = d + dis;
+                }
+            }
+        }
+        int res{};
+        for (int d : distances) {
+            if (d == INF) return -1;
+            res = max(res, d);
+        }
+
+        return res;
+    }
+};
