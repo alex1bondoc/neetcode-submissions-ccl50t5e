c6961af3b18a9948d894b97f8e4842d0461c9f1a
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-08-28 13:09:57 +0300

    Add: meeting-schedule-ii - submission-15

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-15.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+            return a.end == b.end ? a.start < b.start : a.end < b.end;
+        });
+        int start{};
+        int end{};
+        int res{}, count{};
+        while (start < intervals.size() && end < intervals.size()) {
+            if (intervals[start].start < intervals[end].end){
+                count ++;
+                start ++;
+            }
+            else {
+                count --;
+                end ++;
+            }
+            res = max(res, count);
+        }
+        return count;
+    }
+};
