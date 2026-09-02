6a58836bc62156ea90512b5aeb38f9f8b17f7ad6
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-09-02 15:06:07 +0300

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.cpp
@@ -0,0 +1,30 @@
+class LRUCache {
+    int capacity;
+    std::list<int> used;
+    std::unordered_map<int, std::pair<int, list<int>::iterator>> cache;
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if (!cache.contains(key)) {
+            return -1;
+        }
+        used.erase(cache[key].second);
+        used.push_back(key);
+        cache[key] = {cache[key].first, --used.end()};
+        return cache[key].first;
+    }
+    
+    void put(int key, int value) {
+        if (cache.contains(key)) cache.erase(key);
+        else if (used.size() == capacity) {
+            int val{used.front()};
+            used.erase(used.begin());
+            cache.erase(val);
+        }
+        used.push_back(key);
+        cache[key] = {value, --used.end()};
+    }
+};
