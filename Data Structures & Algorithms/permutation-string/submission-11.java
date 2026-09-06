3cc4fa27f6b5a7bc3529b19ce6e7890e6bffe080
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-09-06 14:11:18 +0300

    Add: permutation-string - submission-11

---
+++ Data Structures & Algorithms/permutation-string/submission-11.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] count = new int[26];
+        int[] count2 = new int[26];
+        int need = 0;
+        for (char c : s1.toCharArray()) {
+            if (count[c - 'a'] == 0) need ++;
+            count[c - 'a']++;
+        }
+        int l = 0;
+        for (int r = 0; r < s2.length(); r++) {
+            char c = s2.charAt(r);
+            count2[c - 'a']++;
+            if (count2[c - 'a'] == count[c - 'a']) need --;
+            else if (count2[c - 'a'] == count[c - 'a'] + 1) need ++;
+            if (r >= s1.length()) {
+                c = s2.charAt(l);
+                count2[c - 'a']--;
+                if (count2[c - 'a'] == count[c - 'a']) need --;
+                else if (count2[c - 'a'] == count[c - 'a'] - 1) need ++;
+                l++;
+            }
+
+            if (need == 0) return true;
+        }
+        return false;
+
+    }
+}
